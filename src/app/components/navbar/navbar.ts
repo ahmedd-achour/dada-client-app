bcbb4e4fc1c767e620b5fe239add46f17fd720ed
Ahmed Achour <achourahmed709@gmail.com>
2026-09-13 10:44:19 +0100

    version proffesional dada , added nav bar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { BUSINESS_INFO, waLink } from '../../shared/business-info';
 
 @Component({
@@ -9,4 +9,15 @@ import { BUSINESS_INFO, waLink } from '../../shared/business-info';
 export class Navbar {
   protected readonly phone = BUSINESS_INFO.phone;
   protected readonly whatsappLink = waLink('Bonjour, je souhaite avoir des informations sur vos voitures de location.');
+  protected readonly menuOpen = signal(false);
+
+  protected toggleMenu(): void {
+    this.menuOpen.update((open) => !open);
+    document.body.style.overflow = this.menuOpen() ? 'hidden' : '';
+  }
+
+  protected closeMenu(): void {
+    this.menuOpen.set(false);
+    document.body.style.overflow = '';
+  }
 }
